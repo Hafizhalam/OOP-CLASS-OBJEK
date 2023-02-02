5450fa11d6ed0712f775d5871f723957134b5880
Hafizhalam <124125726+Hafizhalam@users.noreply.github.com>
2023-02-02 11:26:54 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using apa;
+
+Kalkulator hitung = new Kalkulator();
+
+// hitung.tambah();
+// hitung.kurang();
+
+Console.Write("masukan alas : ");
+int a = Convert.ToInt32( Console.ReadLine() );
+Console.Write("masukan tinggi : ");
+int t = Convert.ToInt32( Console.ReadLine() );
+
+Rumus rumus = new Rumus();
+int luas = rumus.jajargenjang (a,t);
+
+Console.WriteLine($"luas jajargenjang dengan a ={a} dan t={t} adalah {luas}");
---
+++ apa.cs
@@ -0,0 +1,17 @@
+namespace apa
+{
+class Kalkulator
+{
+public void tambah(){
+    Console.WriteLine("tambah");}
+public void kurang(){
+    Console.WriteLine("kurang");}
+}
+class Rumus
+{
+public void lingkaran(){
+Console.WriteLine("lingkaran");}
+public int jajargenjang(int a,int t){
+return a*t;}
+}
+}
